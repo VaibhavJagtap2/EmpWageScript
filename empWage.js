e23cc31c1f017d041fb483bfa1229476f4d4f315
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-23 22:02:29 +0530

    Add:Wages According To Hrs

--- empWage.js
+++ empWage.js
@@ -1,7 +1,32 @@
+//UC1 if else Condition
+{
 const IS_ABSENT = 0
 let empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == IS_ABSENT) {
-    console.log("UC1 - Employee is ABSENT. Exiting The Program");
+    console.log("UC1 - Employee is ABSENT");
 } else {
     console.log("UC1 - Employee is PRESENT");
+    }
 }
+
+//UC2 Switch Case
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HRS;
+        break;
+    default:        
+}
+
+let empWage = empHrs * WAGE_PER_HR;
+console.log("UC2 - Emp Wage: " + empWage);
