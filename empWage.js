6e30ad7b39005b3c7e9fcaf60eba0f6b0c7f851c
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-26 12:18:50 +0530

    Add:Wage For PartTime and FullTime

--- empWage.js
+++ empWage.js
@@ -1,32 +1,23 @@
-//UC1 if else Condition
-{
-const IS_ABSENT = 0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("UC1 - Employee is ABSENT");
-} else {
-    console.log("UC1 - Employee is PRESENT");
-    }
-}
-
-//UC2 Switch Case
+//UC3 Switch Case
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
+function getWorkingHours(empCheck) {
 switch (empCheck) {
     case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
-        break;
+        return PART_TIME_HRS;
     case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        break;
+        return FULL_TIME_HRS;
     default:        
+        return 0;
+    }
 }
-
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HR;
 console.log("UC2 - Emp Wage: " + empWage);
+
+
